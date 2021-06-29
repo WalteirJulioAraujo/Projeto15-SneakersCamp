b68fc56ec0308cf4145d138500ba1c0559994296
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-06-29 12:17:20 -0300

    estrutura inicial

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,74 @@
-export default function SignUp(){
-    return(
-        <div>
-            SignUp
-        </div>
+import { useState } from "react";
+import styled from "styled-components";
+
+export default function SignUp() {
+    const [ name, setName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ confirmPassword, setConfirmPassword ] = useState("");
+    const [ cep, setCep ] = useState("");
+    const [ disabled, setDisabled ] = useState(false);
+    function sendForms() {
+
+        return alert("teste");
+    }
+
+    return (
+        <InputFields>
+            <form onSubmit={sendForms}>
+                <input
+                    type="text"
+                    placeholder="Nome"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    disabled={disabled}
+                    required
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    disabled={disabled}
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    disabled={disabled}
+                    required
+                />
+                <input
+                    type="password"
+                    placeholder="Confirme a senha"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    disabled={disabled}
+                    required
+                />
+                <input
+                    type="text"
+                    placeholder="CEP do endereço"
+                    value={cep}
+                    onChange={(e) => setCep(e.target.value)}
+                    disabled={disabled}
+                    required
+                />
+                <button type="submit" disabled={true}>
+                    {" "}
+                    Cadastrar{" "}
+                </button>
+            </form>
+        </InputFields>
     );
 }
+
+const InputFields = styled.div`
+    display: flex;
+    input {
+        display: block;
+        border: none;
+    }
+`;
