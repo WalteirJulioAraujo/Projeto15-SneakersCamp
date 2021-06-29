6c3af2f03162b2a7a451d373e9f48157414da3bb
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-06-29 17:10:51 -0300

    ajustando configuraçoes

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,9 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
     .body{
-
+        font-family: 'Raleway', sans-serif;
     }
-
 `;
 
 export default GlobalStyle;
