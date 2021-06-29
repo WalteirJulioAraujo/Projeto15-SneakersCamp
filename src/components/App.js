a20d5908ea5925409adf25cc6be7aaa8906efb0a
Fernando Valani Cavalcante <valanife@gmail.com>
2021-06-29 18:31:05 -0300

    feature navbar implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,29 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './Home.js';
-import LogIn from './LogIn.js';
-import SignUp from './SignUp.js';
-import Cart from './Cart.js';
-import GlobalStyle from '../styles/GlobalStyles.js';
-import UserContext from '../contexts/UserContext.js';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Home.js";
+import LogIn from "./LogIn.js";
+import SignUp from "./SignUp.js";
+import Cart from "./Cart.js";
+import Navbar from "./Navbar.js";
+import GlobalStyle from "../styles/GlobalStyles.js";
+import UserContext from "../contexts/UserContext.js";
 
-export default function App(){
-    return(
-        <>
-            <UserContext.Provider value=''>
-                <BrowserRouter>
-                    <Switch>
-                        <Route path='/' exact component={Home} />
-                        <Route path='/login' exact component={LogIn} />
-                        <Route path='/signup' exact component={SignUp} />
-                        <Route path='/cart' exact component={Cart} />
-                    </Switch>
-                </BrowserRouter>
-                <GlobalStyle/>
-            </UserContext.Provider>
-        </>
-    );
+export default function App() {
+  return (
+    <>
+      <UserContext.Provider value="">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact>
+              <Navbar />
+              <Home />
+            </Route>
+            <Route path="/login" exact component={LogIn} />
+            <Route path="/signup" exact component={SignUp} />
+            <Route path="/cart" exact component={(Navbar, Cart)} />
+          </Switch>
+        </BrowserRouter>
+        <GlobalStyle />
+      </UserContext.Provider>
+    </>
+  );
 }
