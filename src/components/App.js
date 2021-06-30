1edf1ab37a18f056d70c5926da5b83d8c9058e21
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-06-30 10:25:31 -0300

    Setup completo do LogIn

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useState } from 'react';
 import Home from "./Home.js";
 import LogIn from "./LogIn.js";
 import SignUp from "./SignUp.js";
@@ -8,16 +9,21 @@ import GlobalStyle from "../styles/GlobalStyles.js";
 import UserContext from "../contexts/UserContext.js";
 
 export default function App() {
+
+  const [ user, setUser ] = useState(undefined);
+
   return (
     <>
-      <UserContext.Provider value="">
+      <UserContext.Provider value={{ user, setUser }}>
         <BrowserRouter>
           <Switch>
             <Route path="/" exact>
               <Navbar />
               <Home />
             </Route>
-            <Route path="/login" exact component={LogIn} />
+            <Route path="/login" exact>
+              <LogIn/>
+            </Route>
             <Route path="/signup" exact component={SignUp} />
             <Route path="/cart" exact component={(Navbar, Cart)} />
           </Switch>
